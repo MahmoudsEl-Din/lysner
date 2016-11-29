9d0cad2119603d89b28f347f3107fb0d640e9b41
MahmoudsEl-Din <m.s.el-din@hotmail.com>
2016-11-29 03:27:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var PartnerListener = function () {
     };
 };
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || 8080,
+    ip   = process.env.IP   || '0.0.0.0';
 
 var app;
 if (process.env.NODE_ENV !== 'production') {
